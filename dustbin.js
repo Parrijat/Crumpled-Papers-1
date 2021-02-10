b36a4a2b8611ebf4f6f7648d58dd68deda5a4df2
Parrijat <74194528+Parrijat@users.noreply.github.com>
2021-02-10 17:07:35 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -3,17 +3,18 @@ class Dustbin{
         var options = {
             isStatic:true
         }
-        this.body1 = Bodies.rectangle(900,440,20,100);
-        this.body2 = Bodies.rectangle(700,440,20,100);
-        this.body3 = Bodies.rectangle(800,480,181,20);
+        this.body1 = Bodies.rectangle(900,420,20,100);
+        this.body2 = Bodies.rectangle(700,420,20,100);
+        this.body3 = Bodies.rectangle(800,460,181,20);
         World.add(world,this.body1);
         World.add(world,this.body2);
         World.add(world,this.body3);
     
     }
     display(){
-        rect(900,430,20,100);
-        rect(700,430,20,100);
-        rect(800,470,181,20);
+        fill("white");
+        rect(900,420,20,100);
+        rect(700,420,20,100);
+        rect(800,460,181,20);
     }
 }
